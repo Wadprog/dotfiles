b6922f109c3a0bd4c088a76a7b9b753c7d7f82ec
Wadson Vaval <vavalson@hotmail>
2025-12-06 23:10:23 -0500

    change terminal font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.font = wezterm.font('FiraCode Nerd Font Mono')
-config.font_size = 16.0
+config.font_size = 17.0
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
